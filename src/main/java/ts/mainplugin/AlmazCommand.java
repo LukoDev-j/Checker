748ab1cb7b474fce9bf1c4b089f4ccea65abb82f
LukovskyDev <sillyfis@icloud.com>
2024-12-27 16:15:03 +0300

    плагин

--- src/main/java/ts/mainplugin/AlmazCommand.java
+++
@@ -1,8 +0,0 @@
-package ts.mainplugin;
-
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-
-public interface AlmazCommand {
-    boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings);
-}
